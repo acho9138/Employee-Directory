b7de0fac78a6288137cb7fa4aa6f8787b87eaa15
Anna C <anna.s.chong@gmail.com>
2020-09-29 13:10:20 +1000

    MOVE data file

---
+++ src/utils/Data.js
@@ -0,0 +1,68 @@
+export const data = [
+  {
+    name: 'John Tucker',
+    phone: '0423 347 092',
+    email: 'john.tucker@gmail.com',
+    dob: '23/09/1989'
+  },
+  {
+    name: 'Rob Marsh',
+    phone: '0486 974 285',
+    email: 'rob.marsh@gmail.com',
+    dob: '25/08/1978'
+  },
+  {
+    name: 'Fiona Chen',
+    phone: '0417 204 285',
+    email: 'fiona.chen@gmail.com',
+    dob: '17/01/1990'
+  },
+  {
+    name: 'Mary De Souza',
+    phone: '0462 851 280',
+    email: 'mary.desouza@gmail.com',
+    dob: '14/12/1995'
+  },
+  {
+    name: 'Ashton Cox',
+    phone: '0456 341 936',
+    email: 'ashton.cox@gmail.com',
+    dob: '10/12/1992'
+  },
+  {
+    name: 'Rhona Davidson',
+    phone: '0468 239 746',
+    email: 'rhona.davidson@gmail.com',
+    dob: '23/07/1991'
+  },
+  {
+    name: 'Sonya Frost',
+    phone: '0472 882 343',
+    email: 'sonya.frost@gmail.com',
+    dob: '14/06/1988'
+  },
+  {
+    name: 'Michael Little',
+    phone: '0485 443 959',
+    email: 'michael.little@gmail.com',
+    dob: '20/08/1996'
+  },
+  {
+    name: 'Jenette Caldwell',
+    phone: '0482 221 882',
+    email: 'jenette.caldwell@gmail.com',
+    dob: '21/04/1992'
+  },
+  {
+    name: 'Yuri Berry',
+    phone: '0488 449 020',
+    email: 'yuri.berry@gmail.com',
+    dob: '11/01/1987'
+  },
+  {
+    name: 'William Anderson',
+    phone: '0480 182 028',
+    email: 'william.anderson@gmail.com',
+    dob: '30/08/1988'
+  }
+]
