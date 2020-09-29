dcb20610e67d5c8bb58a38e3f8ce2763d270b958
Anna C <anna.s.chong@gmail.com>
2020-09-29 11:16:08 +1000

    ADD caret buttons

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import { data } from './Data'
-
-const styles = {
-  textAlign: 'center'
-}
+import React, { useContext } from 'react';
+import { data } from './Data';
+import CaretButton from './CaretButton';
 
 function EmployeeList() {
+  // const [employee, setEmployee] = useContext(EmployeeContext);
+
   return (
-    <div className="container table-responsive-lg" style={styles}>
+    <div className="container table-responsive-lg" style={{ textAlign: 'center' }}>
       <table className="table table-striped">
         <thead>
           <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Phone</th>
-            <th scope="col">Email</th>
-            <th scope="col">DOB</th>
+            <th scope="col">Name <CaretButton onClick={() => { }} /> </th>
+            <th scope="col">Phone <CaretButton onClick={() => { }} /> </th>
+            <th scope="col">Email <CaretButton onClick={() => { }} /> </th>
+            <th scope="col">DOB <CaretButton onClick={() => { }} /> </th>
           </tr>
         </thead>
         <tbody>
           {
-            data.map((item, i) => {
+            data.map((item, index) => {
               return (
-                <tr key={i + item.name}>
+                <tr key={index + item.name}>
                   <th scope="row">{item.name}</th>
                   <td>{item.phone}</td>
                   <td>{item.email}</td>
