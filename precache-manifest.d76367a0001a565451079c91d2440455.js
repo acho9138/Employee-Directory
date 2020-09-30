f75eca241f4fa2ac9ce87bcded0edebb43904845
Anna C <anna.s.chong@gmail.com>
2020-09-30 22:09:20 +1000

    Updates

---
+++ precache-manifest.d76367a0001a565451079c91d2440455.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39645661957d6331fa89cee23fbb48d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f54e9c6cca2e770a6b6",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4f54e9c6cca2e770a6b6",
+    "url": "/static/js/2.f45f899d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f45f899d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99fdd08325d68dba99ab",
+    "url": "/static/js/main.121dc742.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
--- src/components/CaretButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function CaretButton(props) {
-  return (
-    <i className="fa fa-caret-down" onClick={props.onClick} style={{ cursor: 'pointer' }} />
-  )
-}
-
-export default CaretButton;
